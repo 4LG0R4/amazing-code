bbbfb3cb9a4a532011b78b5dbeed6dcd1b0e53e3
JepangLee <ljsung0805@gmail.com>
2020-04-14 23:25:26 +0900

    Solve 1463

---
+++ 1463/jepanglee.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.min;
+
+public class jepanglee {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int n = 0;
+    n = in.nextInt();
+
+    int[] arr = new int[1000001];
+
+    arr[0] = 0;
+    arr[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+      arr[i] = arr[i - 1] + 1;
+      if (i % 2 == 0) {
+        arr[i] = min(arr[i], arr[i / 2] + 1);
+      }
+      if (i % 3 == 0) {
+        arr[i] = min(arr[i], arr[i / 3] + 1);
+      }
+    }
+
+    System.out.println(arr[n]);
+  }
+}
